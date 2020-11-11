5b6d694a19660d769b3b131dcd4e0c8e28f18afd
heywhereismymind <mayakovskovo99@mail.ru>
2020-11-11 13:53:01 +0200

    comment

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(function () {
+
+    $('.slider').slick({
+        nextArrow: document.querySelector('.right_arrow'),
+        prevArrow: document.querySelector('.left_arrow'),
+        draggable: false,
+        adaptiveHeight: false
+    })
+});
+
+
+$(function () {
+
+    $('.main_gallery').slick({
+        nextArrow: document.querySelector('.right'),
+        prevArrow: document.querySelector('.left'),
+        draggable: false,
+    })
+})
+
+$(function () {
+
+    $('.slide').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        nextArrow: document.querySelector('.right_arrow1'),
+        prevArrow: document.querySelector('.left_arrow1'),
+        responsive: [
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            }
+        ]
+    })
+})
+
+jQuery(function ($) {
+    $("#number").mask("+375 (99) 999-99-99");
+});
