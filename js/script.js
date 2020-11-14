2adf3197c0b6e7742a7a1bdbe54d53430684079c
3a6op <mayakovskovo99@mail.ru>
2020-11-14 18:10:06 +0300

    comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/*---------------Sliders------------------*/
+
 $(function () {
 
     $('.slider').slick({
@@ -39,16 +41,45 @@ $(function () {
     })
 })
 
+/*---------------Feedback------------------*/
 jQuery(function ($) {
     $("#number").mask("+375 (99) 999-99-99");
 });
 
 
+/*---------------Burger Menu------------------*/
 (function ($) {
-    $(function(){
-       $('.burger_icon').on('click', function(){
-           $(this).closest('.nav').toggleClass('nav-open');
-       }); 
+    $(function () {
+        $('.burger_icon').on('click', function () {
+            $(this).closest('.nav').toggleClass('nav-open');
+        });
     });
 })(jQuery);
 
+
+/*---------------Smooth transition by link------------------*/
+$(document).ready(function () {
+    $("#menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
+
+
+/*---------------Return button------------------*/
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
