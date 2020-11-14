b54bb9bca3f2c288df3db11f71bedb14315a677d
3a6op <mayakovskovo99@mail.ru>
2020-11-14 20:42:42 +0300

    comment

--- js/script.js
+++ js/script.js
@@ -83,3 +83,30 @@ btn.on('click', function (e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '300');
 });
+
+
+/*---------------Fixed Menu------------------*/
+$('.bottom_line').addClass('original').clone().insertAfter('.bottom_line').addClass('cloned').css('position', 'fixed').css('top', '0').css('margin-top', '0').css('z-index', '500').css('padding', 0).css('opacity', '0.8').removeClass('original').hide();
+
+scrollIntervalID = setInterval(stickIt, 10);
+
+
+function stickIt() {
+
+    var orgElementPos = $('.original').offset();
+    orgElementTop = orgElementPos.top;
+
+    if ($(window).scrollTop() >= (orgElementTop)) {
+
+        orgElement = $('.original');
+        coordsOrgElement = orgElement.offset();
+        leftOrgElement = coordsOrgElement.left;
+        widthOrgElement = orgElement.css('width');
+        $('.cloned').css('left', leftOrgElement + 'px').css('top', 0).css('width', widthOrgElement).show();
+        $('.original').css('visibility', 'hidden');
+    } else {
+
+        $('.cloned').hide();
+        $('.original').css('visibility', 'visible');
+    }
+}
